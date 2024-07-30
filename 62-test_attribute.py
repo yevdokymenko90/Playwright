7cbab6ebd12f6c208fcab08bc1d59c75dd28a9dd
yevdokymenko90 <yevdokymenko90@gmail.com>
2024-07-30 16:18:47 +0200

    fixes

--- 62-test_attribute.py
+++ 62-test_attribute.py
@@ -1,7 +1,6 @@
 import re
 from playwright.sync_api import Page, expect
 
-
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/python")
     
@@ -17,13 +16,9 @@ def test_get_started_link(page: Page):
         re.compile(r"navbar__link")
     )
 
-    # expect id
-    # expect(locator).to_have_id("value")
+    # expect attribute in locator - checking for the presence of href attribute with a specific value
+    expect(docs_link).to_have_attribute("href", "/python/docs/intro")
 
-    # expect attribute in locator
-    expect(docs_link).to_have_attribute("href")
-
-    # expect attribute and value in locator
-    expect(docs_link).to_have_attribute(
-        "href", "/python/docs/intro"
-    )
+    # If you want to check the presence of an attribute without specifying its value
+    href_value = docs_link.get_attribute("href")
+    assert href_value is not None
