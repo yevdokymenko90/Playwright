c0fc3eb956e1d0b1e483ff5e7caad7b0e08f4e19
yevdokymenko90 <yevdokymenko90@gmail.com>
2024-07-30 22:44:39 +0200

    +

--- 81-overlapped.py
+++ 81-overlapped.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect # type: ignore
 
 
 def test_overlapped(page: Page):
---
+++ models/login_page.py
@@ -0,0 +1,12 @@
+
+from  playwright.sync_api import Page # type: ignore
+
+
+class LoginPage:
+    
+    def __init__(self, page):
+        pass
+    
+    def login(self, username, password):
+        pass
+        
