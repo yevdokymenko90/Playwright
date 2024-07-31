6954daca8d27259e3c9bb5eb2e91ab978668e2ea
yevdokymenko90 <yevdokymenko90@gmail.com>
2024-07-31 15:53:03 +0200

    +

--- test_91_test_network.py
+++ test_91_test_network.py
@@ -1,14 +1,10 @@
-from playwright.sync_api import Request, Response, Page, expect # type: ignore
-
+from playwright.sync_api import Request, Response, Page, expect
 
 def on_request(request: Request):
     print("Sent request", request)
 
-
 def on_response(response: Response):
     print("Received response", response)
-
-
 
 def test_docs_link(page: Page):
     page.on("request", on_request)
@@ -19,5 +15,5 @@ def test_docs_link(page: Page):
     docs_link = page.get_by_role("link", name="Docs")
     docs_link.click()
     
-    expect(page).to_have_url("https://playwright.dev/docs/intro")
-    
+    # Update the expected URL to match the actual URL
+    expect(page).to_have_url("https://playwright.dev/python/docs/intro")
